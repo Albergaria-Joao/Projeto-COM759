2ea7b39ef8f5f10089f609233b6050a18f08200a
João Albergaria <jvalbergariabar@gmail.com>
2025-11-17 10:52:22 -0300

    Delete feito

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -19,7 +19,7 @@ def hashing(password, salt):
 def index():
     if session.get("nome") == None:
         return redirect("/login")
-    return render_template("dashboard.html", usuarios=db.usuario.find().sort("_id", 1))
+    return render_template("dashboard.html", usuarios=db.membro.find().sort("_id", 1))
     #return flask.jsonify(json.loads(json_util.dumps(db.usuario.find({}).sort("_id", 1))))
 
 
@@ -39,6 +39,12 @@ def login():
     else:
         return redirect("/erro")
     
+@app.route('/logout', methods=['POST'])
+def logout():
+    session.pop("nome", None)
+    return jsonify(mensagem='logout realizado')
+
+    
 # @app.route('/erro', methods=['GET', 'POST'])
 # def login():
 #     print("ERRO")
@@ -47,51 +53,45 @@ def login():
 
 
 
-def criar_membro():
+def criar_membro(dados):
     membro = db["membro"]
 
-    password = "joao"
+    password = dados["senha"]
     hashed_password = hashing(password, salt)
+    dados["senha"] = hashed_password
 
-
-    novo_usuario = {
-        "nome": "João",
-        "login": "joao",
-        "email": "joao@gmail.com",
-        "senha": hashed_password
-    }
-
-    resultado = membro.insert_one(novo_usuario)
+    resultado = membro.insert_one(dados)
     #
 
-@app.route('/create', methods=['GET', 'POST'])
+@app.route('/create-membro', methods=['GET', 'POST'])
 def create():
     #
     
     json_data = request.form.to_dict()
-    criar_membro()
+    #criar_membro()
     if request.method == 'GET':
         return render_template("create.html")
     if json_data is not None:
-        db.usuario.insert_one(json_data)
+        #db.membro.insert_one(json_data)
+        criar_membro(json_data)
         return jsonify(mensagem='usuario criado')
     else:
         return jsonify(mensagem='usuario não criado')
     
-@app.route("/getid/<string:userId>")
-def getid(userId):
-    usuario = db.usuario.find_one({"_id": ObjectId(userId)})
+@app.route("/get-membro/<string:membroId>")
+def getid(membroId):
+    usuario = db.membro.find_one({"_id": ObjectId(membroId)})
     return flask.jsonify(json.loads(json_util.dumps(usuario)))
 
-@app.route("/delete/<string:userId>")
-def delete(userId):
-    result = db.usuario.delete_one({"_id": ObjectId(userId)})
+@app.route("/delete-membro/<string:membroId>", methods=['POST'])
+def delete(membroId):
+    result = db.membro.delete_one({"_id": ObjectId(membroId)})
     if(result.deleted_count > 0):
         return jsonify(mensagem='usuario removido')
     else:
         return jsonify(mensagem='usuario não removido')
 
-@app.route('/update', methods=['POST'])
+@app.route('/update-membro', methods=['POST'])
 def update():
     json_data = request.form.to_dict()
     if json_data is not None and db.usuario.find_one({"_id": ObjectId(json_data["id"])}) is not None:
